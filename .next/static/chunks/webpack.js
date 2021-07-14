7d3d3e4be1c176fbbdba8e079d31c57a2fe8436e
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-13 23:22:22 -0400

    added head section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91fc92faebd1b1f64789"; }
+/******/ 		__webpack_require__.h = function() { return "40f7fa2ad9bb28900585"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,28 @@
 import React from 'react';
 
+import Head from 'next/head';
+
 const Layout = ({ children }) => {
+    const head = () => (
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+              integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+              crossOrigin="anonymous"/>
+    );
+
     const nav = () => (
-        <ul className="nav">
+        <ul className="nav nav-tabs bg-warning">
             <li className="nav-item">
-                <a className="nav-link" href="">
+                <a className="nav-link text-dark" href="">
                     Home
                 </a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="">
+                <a className="nav-link text-dark" href="">
                     Login
                 </a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="">
+                <a className="nav-link text-dark" href="">
                     Register
                 </a>
             </li>
@@ -23,7 +31,7 @@ const Layout = ({ children }) => {
 
     return (
         <React.Fragment>
-            {nav()} {children}
+            {head()} {nav()} <div className="container pt-5 pb-5">{children}</div>
         </React.Fragment>
     );
 };
