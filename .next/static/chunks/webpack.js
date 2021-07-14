47ee3b4f45edaaf15be5ac11df557a827e4dfd18
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-13 23:43:50 -0400

    added public css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1993f375739e8a11dd60"; }
+/******/ 		__webpack_require__.h = function() { return "60b2289667fbe73fe799"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
