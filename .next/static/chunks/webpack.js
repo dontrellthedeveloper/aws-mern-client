831f7fda96e3443c9268dc5341e59bdf8b0c5d46
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-13 23:29:15 -0400

    added link component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "40f7fa2ad9bb28900585"; }
+/******/ 		__webpack_require__.h = function() { return "85fe7a9b9b11c2c2e18c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
